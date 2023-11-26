f0385ee440ed30c60c571d212daedae4f91dda39
SeeuSim <seeusimong@gmail.com>
2023-11-26 12:34:36 +0800

    Add dark mode toggle
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- src/components/sections/nav.tsx
+++ src/components/sections/nav.tsx
@@ -1,3 +1,4 @@
+import { DarkModeToggle } from '@/components/toggles/dark-mode';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 
@@ -5,15 +6,18 @@ export const Nav = () => {
 	return (
 		<nav className='flex max-h-min w-full border-b border-border px-20 py-[30px]'>
 			<div className='flex w-full justify-between'>
-				<Button variant='secondary'>
+				<Button variant='secondary' className='text-base font-semibold'>
 					<span>Logo</span>
 				</Button>
 				<Badge variant='outline' className='hidden !py-1 px-4 text-lg font-black md:flex'>
 					<span>About</span>
 				</Badge>
-				<Button variant='default'>
-					<span>Connect</span>
-				</Button>
+				<div className='inline-flex items-center gap-2'>
+					<DarkModeToggle />
+					<Button variant='destructive' className='text-base font-semibold'>
+						<span>Connect</span>
+					</Button>
+				</div>
 			</div>
 		</nav>
 	);
---
+++ src/components/toggles/dark-mode.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { MoonStar, Sun } from 'lucide-react';
+import { useEffect, useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+const LS_DARK_MODE_KEY = 'darkMode';
+const DARK_MODE_QUERY = '(prefers-color-scheme: dark)';
+
+export const DarkModeToggle = () => {
+	const [isDarkMode, _setIsDarkMode] = useState(false);
+
+	const setIsDarkMode = (_isDarkMode: boolean) => {
+		const root = document.documentElement;
+		if (_isDarkMode) {
+			root.classList.add('dark');
+			localStorage.setItem(LS_DARK_MODE_KEY, 'true');
+		} else {
+			root.classList.remove('dark');
+			localStorage.setItem(LS_DARK_MODE_KEY, 'false');
+		}
+		_setIsDarkMode(_isDarkMode);
+	};
+
+	useEffect(() => {
+		if (window) {
+			window.matchMedia(DARK_MODE_QUERY).addEventListener('change', (event) => {
+				setIsDarkMode(event.matches);
+			});
+			if (
+				localStorage.getItem(LS_DARK_MODE_KEY) === 'true' ||
+				window.matchMedia(DARK_MODE_QUERY).matches
+			) {
+				setIsDarkMode(true);
+			}
+		}
+	}, []);
+
+	return (
+		<Button
+			id='DarkModeToggle'
+			variant='outline'
+			className={cn(
+				'h-11 w-11 overflow-hidden rounded-md border p-0 transition-colors ease-in-out',
+				'border-violet-600 text-violet-600 dark:border-yellow-600 dark:text-yellow-600',
+				'hover:bg-violet-600 hover:text-violet-100 dark:hover:bg-yellow-600 dark:hover:text-yellow-100'
+			)}
+			onClick={() => {
+				setIsDarkMode(!isDarkMode);
+			}}
+		>
+			<div
+				id='IconSlider'
+				className={cn(
+					'inline-flex items-center transition-transform duration-500 ease-in-out ',
+					'translate-x-[22px] gap-[19px] dark:translate-x-[-22px]'
+				)}
+			>
+				<MoonStar className='' />
+				<Sun className='' />
+			</div>
+			<span className='sr-only' id='ScreenReaderLabel'>
+				Toggle Dark Mode
+			</span>
+		</Button>
+	);
+};
