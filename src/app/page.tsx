b90128db7d153ecb100f5df7a3244f872d09ddef
SeeuSim <seeusimong@gmail.com>
2023-11-26 10:51:28 +0800

    Change prettier fmt rules
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Nav } from '@/components/sections/nav';
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen">
+		<main className='flex min-h-screen'>
 			<Nav />
 		</main>
 	);
--- src/components/sections/nav.tsx
+++ src/components/sections/nav.tsx
@@ -3,15 +3,15 @@ import { Button } from '@/components/ui/button';
 
 export const Nav = () => {
 	return (
-		<nav className="flex max-h-min w-full border-b border-border px-20 py-[30px]">
-			<div className="flex w-full justify-between">
-				<Button variant="secondary">
+		<nav className='flex max-h-min w-full border-b border-border px-20 py-[30px]'>
+			<div className='flex w-full justify-between'>
+				<Button variant='secondary'>
 					<span>Logo</span>
 				</Button>
-				<Badge variant="outline" className="hidden !py-1 px-4 text-lg font-black md:flex">
+				<Badge variant='outline' className='hidden !py-1 px-4 text-lg font-black md:flex'>
 					<span>About</span>
 				</Badge>
-				<Button variant="default">
+				<Button variant='default'>
 					<span>Connect</span>
 				</Button>
 			</div>
