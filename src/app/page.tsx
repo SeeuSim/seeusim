3da02af572080e776c07226206badf46d9a5a4f4
SeeuSim <seeusimong@gmail.com>
2023-12-02 16:37:12 +0800

    Optimise placement of components in nav
    - Shifted position logic to custom hook
    - Shifted consumer logic from `useState` to Context API
    - Added transition smoothing using padding instead of scale
    
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,27 @@
-import { Hero } from '@/components/sections/hero';
-import { Nav } from '@/components/sections/nav';
+import { Hero } from '@/components/sections/home/hero';
+import { Nav } from '@/components/sections/common/nav';
+
+import { cn } from '@/lib/utils';
 
 export default function Home() {
 	return (
-		<main className='relative z-0 mx-auto mt-[100px] flex min-h-screen max-w-screen-2xl flex-col'>
+		<main className={cn('relative z-0 mx-auto flex min-h-screen max-w-screen-2xl flex-col')}>
 			<Nav />
-			<Hero />
-			{Array(100)
-				.fill('Random Text')
-				.map((val, index) => (
-					<div key={index} className='flex w-full'>
-						<span className='mx-auto -translate-x-1/4 text-center'>{val}</span>
-					</div>
-				))}
+			<div
+				className={cn(
+					'translate-y-[100px] transition-transform ease-in-out'
+					// isCompactDisplay && 'translate-y-[72px]'
+				)}
+			>
+				<Hero />
+				{Array(100)
+					.fill('Random Text')
+					.map((val, index) => (
+						<div key={index} className='flex w-full'>
+							<span className='mx-auto -translate-x-1/4 text-center'>{val}</span>
+						</div>
+					))}
+			</div>
 		</main>
 	);
 }
---
+++ src/lib/contexts/navCompactDisplayContext.ts
@@ -0,0 +1,13 @@
+import { createContext, useContext } from 'react';
+
+// Nav AltStyle
+interface INavCompactDisplayState {
+	isCompactDisplay: boolean;
+}
+
+const useNavCompactDisplayContext = createContext<INavCompactDisplayState>({
+	isCompactDisplay: false
+});
+
+export const NavCompactDisplayStateProvider = useNavCompactDisplayContext.Provider;
+export const useNavCompactDisplayState = () => useContext(useNavCompactDisplayContext);
