b5f817178fd07ea5a7eb918e8702e7f8618cc9bf
SeeuSim <seeusimong@gmail.com>
2025-08-31 11:58:09 +0800

    Fix eslint config
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- app/root.tsx
+++ app/root.tsx
@@ -11,6 +11,7 @@ import type { Route } from './+types/root';
 import './app.css';
 
 export const links: Route.LinksFunction = () => [
+  { rel: 'icon', href: '/favicon.svg' },
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
   {
     rel: 'preconnect',
@@ -19,7 +20,7 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
+    href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap',
   },
 ];
 
--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,9 @@
 import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import pluginImport from 'eslint-plugin-import';
+import pluginReact from 'eslint-plugin-react';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
@@ -19,9 +20,51 @@ export default defineConfig([
   },
   {
     ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: { version: 'detect' },
+    },
     rules: {
       ...pluginReact.configs.flat.recommended.rules,
       'react/react-in-jsx-scope': 'off',
+    },
+  },
+  {
+    ...pluginImport.flatConfigs.recommended,
+    rules: {
+      ...pluginImport.flatConfigs.recommended.rules,
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin', // Node
+            'external', // external
+            'internal', // internal path alias
+            ['parent', 'sibling', 'index'], // relative imports
+            'object', // import * as React from 'react';
+            'type',
+          ],
+          pathGroups: [
+            {
+              pattern: '~/**',
+              group: 'internal',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          'newlines-between': 'always',
+        },
+      ],
+      'import/no-unresolved': [
+        'error',
+        {
+          ignore: [
+            '^@react-router/.*', // react-router packages
+          ],
+        },
+      ],
     },
   },
 ]);
